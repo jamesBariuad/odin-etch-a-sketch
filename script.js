76ad59fb77c9174e2b5bad5d1166849341f7e7a4
jokandro <114486039+jokandro@users.noreply.github.com>
2023-06-17 15:00:06 +0800

    feat: change color, rainbow, erase, clear

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let sideLength = 16;
+let drawColor = "black";
+let mode = "default";
 const container = document.querySelector(".container");
 const dimensionButton = document.querySelector("#dimension-button");
 
@@ -8,29 +10,64 @@ const makePixels = () => {
     "style",
     `height:${800 / sideLength}px; width: ${
       800 / sideLength
-    }px; background-color: black;`
+    }px; background-color: whitesmoke;`
   );
   pixel.setAttribute("class", "pixels");
   return pixel;
 };
 
-const fillContainerWithSquares = (sideLength, makePixels) => {
+const fillContainerWithPixels = (sideLength, makePixels) => {
   for (i = 0; i < sideLength * sideLength; i++) {
     container.appendChild(makePixels.cloneNode());
   }
-  addHoverListenerToSquares();
+  addListenersToPixels();
 };
 
-const changeColor = (e) => {
-  e.target.style.backgroundColor = "purple";
-};
-
-const addHoverListenerToSquares = () => {
+const addListenersToPixels = () => {
+  let isDrawing = false;
   const pixels = document.getElementsByClassName("pixels");
   for (item of pixels) {
-    item.addEventListener("mouseover", changeColor);
+    item.addEventListener("mousedown", (e) => {
+      isDrawing = true;
+      if (mode == "default") {
+        e.target.style.backgroundColor = drawColor;
+      } else if (mode == "rainbow") {
+        e.target.style.backgroundColor = `#${Math.floor(
+          Math.random() * 16777215
+        ).toString(16)}`;
+      } else {
+        e.target.style.backgroundColor = "whitesmoke";
+      }
+    });
+    item.addEventListener("mouseenter", (e) => {
+      if (isDrawing) {
+        if (mode == "default") {
+          e.target.style.backgroundColor = drawColor;
+        } else if (mode == "rainbow") {
+          e.target.style.backgroundColor = `#${Math.floor(
+            Math.random() * 16777215
+          ).toString(16)}`;
+        } else {
+          e.target.style.backgroundColor = "whitesmoke";
+        }
+      }
+    });
+    item.addEventListener("mouseup", () => {
+      isDrawing = false;
+    });
   }
 };
+
+// const changeColor = (e) => {
+//   e.target.style.backgroundColor = "purple";
+// };
+
+// const addHoverListenerToPixels = () => {
+//   const pixels = document.getElementsByClassName("pixels");
+//   for (item of pixels) {
+//     item.addEventListener("mouseover", changeColor);
+//   }
+// };
 
 const clearContainer = () => {
   container.replaceChildren();
@@ -42,20 +79,34 @@ const showPrompt = () => {
     return showPrompt();
   }
 
-  if (sideLength > 100 || sideLength===0) {
+  if (sideLength > 100 || sideLength === 0) {
     return showPrompt();
   }
-
-  
-
   changeDimensions();
 };
 
 const changeDimensions = () => {
   clearContainer();
-  fillContainerWithSquares(sideLength, makePixels());
+  fillContainerWithPixels(sideLength, makePixels());
 };
 
 dimensionButton.addEventListener("click", showPrompt);
 
-fillContainerWithSquares(sideLength, makePixels());
+const input = document.querySelector("input");
+const randomColorButton = document.querySelector("#randomColor");
+const eraseButton = document.querySelector("#erase");
+const clear = document.querySelector("#clear");
+
+randomColorButton.addEventListener("click", () => (mode = "rainbow"));
+eraseButton.addEventListener("click", () => (mode = "erase"));
+clear.addEventListener("click", () => handleClear());
+
+const handleClear = () => {
+  return clearContainer(), fillContainerWithPixels(sideLength, makePixels());
+};
+input.addEventListener("change", (e) => {
+  mode = "default";
+  drawColor = e.target.value;
+});
+
+fillContainerWithPixels(sideLength, makePixels());
